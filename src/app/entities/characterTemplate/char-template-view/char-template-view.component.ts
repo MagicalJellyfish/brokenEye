9d7143d17514fe38ac286ae3da6a5afbe2d564a6
MagicalJellyfish <lupe030803@gmail.com>
2024-02-22 22:27:14 +0100

    added some missing fields and stats view to charTemplate view

--- src/app/entities/characterTemplate/char-template-view/char-template-view.component.ts
+++ src/app/entities/characterTemplate/char-template-view/char-template-view.component.ts
@@ -2,9 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatTab } from '@angular/material/tabs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { CharacterTemplate } from 'src/app/api-classes/Characters/CharacterTemplate';
+import { Stat } from 'src/app/api-classes/Stats/Stat';
+import { StatValue } from 'src/app/api-classes/Stats/StatValue';
 import { ConfirmationDialogComponent } from 'src/app/core/confirmation-dialog/confirmation-dialog.component';
 import { RequestService } from 'src/app/services/entities/request/request.service';
 
@@ -17,8 +21,16 @@ export class CharTemplateViewComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, protected requestService: RequestService, private matDialog: MatDialog, private snackBar: MatSnackBar) { }
 
   async ngOnInit(): Promise<void> {
+    (await this.requestService.getAll(this.requestService.routes.constant + "/Stats")).subscribe((stats: any) => {
+      this.allStats = stats
+    });
+
     (await this.requestService.get(this.requestService.routes.characterTemplate, +this.route.snapshot.paramMap.get('id')!)).subscribe((x: any) => {
       this.charTemplate = x
+
+      this.experience = this.charTemplate!.experience
+      this.description = this.charTemplate!.description
+      this.notes = this.charTemplate!.notes
 
       var charCodeString: string = ""
       for(var i = 0; i < this.charTemplate!.image.length; i++) {
@@ -45,13 +57,64 @@ export class CharTemplateViewComponent implements OnInit {
       this.weight.setValue(weight)
       this.age.setValue(age)
       this.isNPC = this.charTemplate!.isNPC;
+
+      this.update()
     })
+
+    this.changeSubject.subscribe(_ => {
+      this.update()
+    })
+  }
+
+  update() {
+    this.statValues = []
+    let statIncreases: StatValue[] = []
+
+    this.charTemplate?.effectTemplates.forEach(effectTemplate => {
+      effectTemplate.statIncreases.forEach(statIncrease => {
+        statIncreases.push(statIncrease)
+      });
+    });
+
+    this.charTemplate?.traitTemplates.forEach(traitTemplate => {
+      traitTemplate.statIncreases.forEach(statIncrease => {
+        statIncreases.push(statIncrease)
+      });
+    });
+
+    this.charTemplate?.itemTemplates.forEach(itemTemplate => {
+      itemTemplate.statIncreases.forEach(statIncrease => {
+        statIncreases.push(statIncrease)
+      });
+    });
+
+    
+    this.allStats.forEach(stat => {
+      let totalIncrease = 0
+      let relevantStatIncreases = statIncreases.filter(x => x.statId == stat.id)
+      relevantStatIncreases.forEach(relevantStatIncrease => {
+        totalIncrease += relevantStatIncrease.value
+      });
+
+      let newValue: StatValue = {
+        id: 0,
+        stat: stat,
+        value: totalIncrease
+      }
+      this.statValues.push(newValue)
+    });
+
+    this.statTable = new MatTableDataSource(this.statValues)
   }
 
   changeSubject: Subject<void> = new Subject()
 
   charTemplate?: CharacterTemplate;
   image: string = ""
+
+  experience: string = ""
+  description: string = ""
+  notes: string = ""
 
   name = new FormControl('', [ Validators.required ])
   height = new FormControl('', [ Validators.pattern("^\\d+(\\.\\d{1,2})?$") ])
@@ -70,6 +133,11 @@ export class CharTemplateViewComponent implements OnInit {
 
   patternMsg = "has to be in format";
 
+  allStats!: Stat[]
+  statValues!: StatValue[]
+  statTable: MatTableDataSource<StatValue> = new MatTableDataSource<StatValue>()
+  statTableCols: string[] = ["name", "value"]
+
   async save() {
     (await this.requestService.patch(this.requestService.routes.characterTemplate, this.charTemplate!.id, JSON.stringify({
       "name": this.name.value,
@@ -77,7 +145,10 @@ export class CharTemplateViewComponent implements OnInit {
       "weight": this.weight.value,
       "age": this.age.value,
       "money": this.money.value,
-      "isNPC": this.isNPC
+      "isNPC": this.isNPC,
+      "experience": this.experience,
+      "description": this.description,
+      "notes": this.notes
     }))).subscribe(_ => this.snackBar.open("Saved changes!", "OK", { duration: 2000 }))
   }
 
