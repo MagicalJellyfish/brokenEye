8f6d83753917d4f265354779049b3a422a28e18b
MagicalJellyfish <lupe030803@gmail.com>
2025-02-12 22:48:17 +0100

    refactor: clear app component ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { SettingsService } from 'src/app/services/settings/settings.service';
-import { UserService } from './services/user/user.service';
-import { TopbarComponent } from './core/topbar/topbar.component';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from './ui/core/topbar/topbar.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    imports: [TopbarComponent, RouterOutlet],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [TopbarComponent, RouterOutlet],
 })
-export class AppComponent implements OnInit {
-  //Initialize settings
-  constructor(
-    private settingsService: SettingsService,
-    private userService: UserService
-  ) {}
-  title = 'brokenEye';
-
-  ngOnInit(): void {}
-}
+export class AppComponent {}
