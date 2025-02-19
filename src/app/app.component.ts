8cc45e85307cc2ac732fed16a18f9329d889c137
MagicalJellyfish <lupe030803@gmail.com>
2025-02-19 23:07:33 +0100

    feat: loading bar when requests are running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { RouterOutlet } from '@angular/router';
+import { LoadingStateService } from './services/loading-state.service';
 import { TopbarComponent } from './ui/core/topbar/topbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [TopbarComponent, RouterOutlet],
+  imports: [TopbarComponent, RouterOutlet, MatProgressBarModule],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(protected loadingStateService: LoadingStateService) {}
+}
